1864c15031cfaaeeb34ed65f9d20ec7157652d0d
elena-fly17 <elena17mr@gmail.com>
2022-02-01 19:02:42 +0200

    Задача решена

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,23 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        AnnotationConfigApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getName());
+
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getName());
+
+        System.out.println("bean1 и bean2 - один и тот же объект? " + (bean1 == bean2));
+        System.out.println("cat1 и cat2 - один и тот же объект? " + (cat1 == cat2));
+        applicationContext.close();
     }
 }
